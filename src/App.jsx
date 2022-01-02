b1eabd29f2fabcad6fd7622ce14b4a0edf98108f
Bruno Conesta <bconesta@etrr.edu.ar>
2022-01-02 03:03:01 -0300

    Card component finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,58 @@
 import './App.css';
 import Card from './components/Card';
+import Navbar from './components/Navbar';
+import Tittle from './components/Tittle';
 import Imagen from './img/7E8A3786.jpg';
 
 function App() {
   return (
-    <>
+    <>      
+      <Navbar></Navbar>
+      <Tittle
+        tittle="Hi, I'm Bruno Conesta!"
+        text="Scroll down to my work"
+        class="tittle"
+      ></Tittle>
       <Card
-        class=""
+        inverted={false}
+        class="card"
         src={Imagen}
         alt="Podras"
-        height="200px"
+        height="350px"
         tittle="All of me"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta commodi et perspiciatis provident dolore quaerat iste dolorum 
-        ducimus minus aliquid, dignissimos cumque porro quia animi consequuntur, blanditiis inventore quos facilis."
+        text="2021"
       ></Card>
+
+      <Card
+        inverted={true}
+        class="card"
+        src={Imagen}
+        alt="Podras"
+        height="350px"
+        tittle="All of me"
+        text="2021"
+      ></Card>
+
+      <Card
+        inverted={false}
+        class="card"
+        src={Imagen}
+        alt="Podras"
+        height="350px"
+        tittle="All of me"
+        text="2021"
+      ></Card>
+
+      <Card
+        inverted={true}
+        class="card"
+        src={Imagen}
+        alt="Podras"
+        height="350px"
+        tittle="All of me"
+        text="2021"
+      ></Card>
+
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 export default function Card(props) {
     const styles = {
                     'container' : {maxHeight : props.height, overflow : 'hidden', display : 'flex', alignItems: 'center'},
-                    'img' : {width : '20%'},
-                    'textcontainer' : {marginLeft : '2%', marginRight : '2%'}, 
+                    'img' : {width : '60%'},
+                    'textcontainer' : {marginLeft : '2%', marginRight : '2%', width : '40%'}, 
                     'text' : {textAlign : 'center'}
                    }
+    
+    if(!props.inverted){
     return (
         <div className={props.class} style={styles.container}>
             <img src={props.src} alt={props.alt} style={styles.img}/>
@@ -16,4 +18,16 @@ export default function Card(props) {
             </div>
         </div>
     )
+    }
+    else{
+    return (
+        <div className={props.class} style={styles.container}>
+            <div style={styles.textcontainer}>
+                <h3 style={styles.text}>{props.tittle}</h3>
+                <p style={styles.text}>{props.text}</p>
+            </div>
+            <img src={props.src} alt={props.alt} style={styles.img}/>
+        </div>
+    )
+    }
 }
